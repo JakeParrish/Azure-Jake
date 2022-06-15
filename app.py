aa1853bd6f4eff67023b83eb7727f6ac6fc15a41
ISeeJp <62587526+ISeeJp@users.noreply.github.com>
2022-06-15 14:58:54 -0400

    form submit page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from dash import dcc, html, Input, Output, callback
 
 
@@ -8,3 +8,8 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return render_template("index.html")
+
+@app.route("/form", methods=["POST"])
+def form():
+    Employee_Name = request.form.get("Employee-Name")
+    return render_template("form.html", name=Employee_Name)
